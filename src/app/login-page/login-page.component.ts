00007b30b38d9187995b9f7c6030fc504f32e4bb
Jaroslav Istok <jaroslavistok@Jaroslavs-MacBook-Pro.local>
2017-04-16 10:01:32 +0200

    Pridany vypis chyb pri prihlasovani a registracii

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,69 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Router } from '@angular/router';
-import { AuthService } from '../providers/auth.service';
+import {Router} from '@angular/router';
+import {AuthService} from '../providers/auth.service';
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
 
-  loginActive: Boolean;
-  registerActive: Boolean;
+    loginActive: Boolean;
+    registerActive: Boolean;
+    displayLogin: String;
+    displayRegister: String;
+    errorMessage: String;
 
-  displayLogin: String;
-  displayRegister: String;
+    constructor(public authService: AuthService, private router: Router) {
+        this.loginActive = true;
+        this.registerActive = false;
+        this.displayLogin = "block";
+        this.displayRegister = "none";
 
-  constructor(public authService: AuthService, private router: Router) {
-    this.loginActive = true;
-    this.registerActive = false;
-    this.displayLogin = "block";
-    this.displayRegister = "none";
-  }
+        this.errorMessage = "";
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  login(userEmail: string, userPassword: string){
-    this.authService.loginWithEmailAndPassword(userEmail, userPassword).then((data) => {
-      console.log(data);
-      this.router.navigate(['']);
-    }).catch((error)=> {
-      console.log("Nespravne zadane informacie" + JSON.stringify(error));
-    });
-  }
+    login(userEmail: string, userPassword: string) {
+        this.authService.loginWithEmailAndPassword(userEmail, userPassword).then((data) => {
+            console.log(data);
+            this.router.navigate(['']);
+        }).catch((error) => {
+            this.setErrorMessage(error);
+        });
+    }
 
-  register(userEmail: string, userPassword: string){
-    this.authService.registerUserWithEmailAndPassword(userEmail, userPassword).then(()=>{
-      this.router.navigate(['home']);
-      this.changeToLogin();
-    }).catch((error)=> {
-      console.log(error);
-      console.log("Error: " + JSON.stringify(error));
-    });
+    register(userEmail: string, userPassword: string) {
+        this.authService.registerUserWithEmailAndPassword(userEmail, userPassword).then(() => {
+            this.router.navigate(['home']);
+            this.changeToLogin();
+        }).catch((error) => {
+            this.setErrorMessage(error);
+        });
+    }
 
-  }
+    setErrorMessage(error) {
+        if (error.code == "auth/wrong-password") {
+            this.errorMessage = "Nesprávne zadané prihlasovacie heslo";
+        } else if (error.code == "auth/invalid-email") {
+            this.errorMessage = "Nesprávne zadaný prihlasovací email";
+        } else if (error.code == "auth/weak-password") {
+            this.errorMessage = "Slabé heslo";
+        } else {
+            this.errorMessage = "Nesprávne zadané prihlasovacie údaje";
+        }
+    }
 
+    changeToLogin() {
+        this.loginActive = true;
+        this.registerActive = false;
+        this.displayLogin = "block";
+        this.displayRegister = "none";
+    }
 
-  changeToLogin(){
-    console.log("ffjirf");
-    this.loginActive = true;
-    this.registerActive = false;
-
-    this.displayLogin = "block";
-    this.displayRegister = "none";
-  }
-
-  changeToRegister(){
-    console.log("efiefwof");
-
-    this.registerActive = true;
-    this.loginActive = false;
-
-    this.displayLogin = "none";
-    this.displayRegister = "block";
-  }
-
+    changeToRegister() {
+        this.registerActive = true;
+        this.loginActive = false;
+        this.displayLogin = "none";
+        this.displayRegister = "block";
+    }
 }
