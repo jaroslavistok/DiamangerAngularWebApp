9047254a4cd8ccb5452500e02a51c55e66e16110
Jaroslav Istok <jaroslavistok@Jaroslavs-MacBook-Pro.local>
2017-04-16 11:52:47 +0200

    Pridana validacia uzivatelskych vstupov

--- src/app/reminders-list/reminders-list.component.ts
+++ src/app/reminders-list/reminders-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../providers/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../providers/auth.service';
 import {FirebaseListObservable, AngularFire, FirebaseObjectObservable} from "angularfire2";
-import { Reminder } from './Reminder';
-import { Ng2SmartTableModule, LocalDataSource } from "ng2-smart-table";
+import {Reminder} from './Reminder';
+import {Ng2SmartTableModule, LocalDataSource} from "ng2-smart-table";
 import {CheckBoxComponentComponent} from "../check-box-component/check-box-component.component";
+import {min} from "rxjs/operator/min";
+import {isNumber} from "util";
+import {isNumeric} from "rxjs/util/isNumeric";
 
 @Component({
-  selector: 'app-reminders-list',
-  templateUrl: './reminders-list.component.html',
-  styleUrls: ['./reminders-list.component.css'],
+    selector: 'app-reminders-list',
+    templateUrl: './reminders-list.component.html',
+    styleUrls: ['./reminders-list.component.css'],
     entryComponents: [
         CheckBoxComponentComponent
     ]
@@ -50,15 +53,15 @@ export class RemindersListComponent implements OnInit {
                 title: 'Meno'
             },
         },
-        pager : {
-            display : true,
-            perPage:50
+        pager: {
+            display: true,
+            perPage: 50
         },
 
         mode: 'inline',
 
 
-        delete : {
+        delete: {
             confirmDelete: true,
             deleteButtonContent: "Vymazať"
         },
@@ -68,7 +71,7 @@ export class RemindersListComponent implements OnInit {
             saveButtonContent: "Uložiť",
             cancelButtonContent: "Zrušiť"
         },
-        add : {
+        add: {
             confirmCreate: true,
             addButtonContent: 'Pridať',
             createButtonContent: 'Vytvoriť',
@@ -76,122 +79,126 @@ export class RemindersListComponent implements OnInit {
         }
     };
 
-    tableData :LocalDataSource;
+    tableData: LocalDataSource;
+    errorMessage: String = "";
 
+    items: FirebaseListObservable<any[]>;
+    private uid: String;
+    private lastReminderIdValue: string;
 
-  items: FirebaseListObservable<any[]>;
-  private uid: String;
-  private lastReminderIdValue: string;
+    public reminder = new Reminder('', '', '', '', false);
+    public lastReminderId: FirebaseObjectObservable<any>;
 
-  public reminder = new Reminder('', '', '', '', false);
-  public lastReminderId: FirebaseObjectObservable<any>;
+    addReminder() {
+        this.lastReminderId.update({'last_reminder_id': this.reminder.id});
+        this.reminder.alarmTime = this.reminder.alarmTime.toString();
+        this.items.push(this.reminder);
+    }
 
-  addReminder(){
-    this.lastReminderId.update({'last_reminder_id': this.reminder.id});
-    this.reminder.alarmTime = this.reminder.alarmTime.toString();
-    this.items.push(this.reminder);
-  }
+    constructor(public authService: AuthService, private router: Router, private angularFire: AngularFire) {
+        this.authService.angularFire.auth.subscribe(
+            (auth) => {
+                if (auth == null) {
+                    this.router.navigate(['login']);
+                } else {
+                    this.uid = auth.uid;
+                    this.items = angularFire.database.list('/users/' + this.uid + "/reminders");
 
-  constructor(public authService: AuthService, private router: Router, private angularFire: AngularFire) {
-    this.authService.angularFire.auth.subscribe(
-        (auth) => {
-          if (auth == null){
-            this.router.navigate(['login']);
-          } else {
-            this.uid = auth.uid;
-            this.items = angularFire.database.list('/users/' + this.uid + "/reminders");
+                    this.items.subscribe(snapshot => {
+                        this.tableData = new LocalDataSource(snapshot);
+                    });
 
-            this.items.subscribe(snapshot => {
-                this.tableData = new LocalDataSource(snapshot);
-            });
+                    this.lastReminderId = angularFire.database.object('/users/' + this.uid);
+                    this.lastReminderId.subscribe(snapshot => {
+                        this.lastReminderIdValue = snapshot.last_reminder_id;
+                    });
 
-            this.lastReminderId = angularFire.database.object('/users/'+ this.uid);
-            this.lastReminderId.subscribe(snapshot => {
-              this.lastReminderIdValue = snapshot.last_reminder_id;
-            });
+                }
+            }
+        );
+    }
 
-          }
+    onCreate(event) {
+        if (this.validateReminderData(event.newData)) {
+
+            let newId = parseInt(this.lastReminderIdValue) + 1;
+            event.newData.id = newId.toString();
+            this.lastReminderId.update({'last_reminder_id': event.newData.id.toString()});
+            this.items.push(event.newData);
+
+            this.errorMessage = "";
+            return event.confirm.resolve(event.newData);
         }
-    );
-  }
 
-  onCreate(event){
-      let newId = parseInt(this.lastReminderIdValue)+1;
-      event.newData.id = newId.toString();
-      this.lastReminderId.update({'last_reminder_id': event.newData.id.toString() });
-      this.items.push(event.newData);
-      return event.confirm.resolve(event.newData);
-  }
+        return event.confirm.reject();
+    }
 
-  validateReminderData(reminderData){
-  /*public name: string,
-          public category: string,
-          public alarmTime: string,
-          public id: string,
-          public active: boolean,*/
-      var valid :boolean = true;
-      if (reminderData.name.length < 1){
-          valid = false
-      }
-      if (reminderData.category.length < 1){
-          valid = false;
-      }
+    validateReminderData(reminderData) {
+        var valid: boolean = true;
+        if (reminderData.name.length < 1) {
+            this.errorMessage = "Meno nesmie byt prazdne!";
+            valid = false
+        }
+        if (reminderData.category.length < 1) {
+            this.errorMessage = "Kategoria nesmie byt prazdna";
+            valid = false;
+        }
+        if (!this.validateTime(reminderData.alarmTime)) {
+            this.errorMessage = "Nekorektne zadany cas";
+            valid = false;
+        }
+        return valid;
+    }
 
+    validateTime(time) {
+        let hoursAndMinutes = time.split(':');
+        let hours = parseInt(hoursAndMinutes[0]);
+        let minutes = parseInt(hoursAndMinutes[1]);
+        if (!isNumeric(hoursAndMinutes[0]) || !isNumeric(hoursAndMinutes[1])){
+            return false;
+        }
+        if (hours < 0 || hours > 24) {
+            return false;
+        }
+        if (minutes < 0 || minutes > 59) {
+            return false;
+        }
+        return true;
+    }
 
+    onEdit(event) {
+        if (this.validateReminderData(event.newData)) {
+            let editedReminder = {
+                category: event.newData.category,
+                active: event.newData.active,
+                alarmTime: event.newData.alarmTime,
+                name: event.newData.name
+            };
 
+            this.items.update(event.data.$key, editedReminder);
 
+            console.log("on update");
 
+            this.errorMessage = "";
+            return event.confirm.resolve(event.data);
+        }
 
-  }
+        return event.confirm.reject();
+    }
 
-  validateTime(time){
-      var hoursAndMinutes = time.split(':');
-      var hours = parseInt(hoursAndMinutes[0]);
-      var minutes = parseInt(hoursAndMinutes[1]);
+    onDelete(event) {
+        if (window.confirm('Prajete si skutocne odstranit zaznam?')) {
+            this.items.remove(event.data);
+            return event.confirm.resolve(event.data);
+        }
+        return event.confirm.reject();
+    }
 
-      console.log("hours: " + hours);
-      console.log("minutes: "+ minutes);
+    ngOnInit() {
+    }
 
-      if (hours < 0 || hours > 24){
-          return false;
-      }
-      if (minutes < 0 || minutes > 59){
-          return false;
-      }
-  }
-
-  onEdit(event){
-
-      window.alert("Bad data");
-
-      return event.confirm.reject();
-
-      // let editedReminder = {
-      //     category: event.newData.category,
-      //     active: event.newData.active,
-      //     alarmTime: event.newData.alarmTime,
-      //     name: event.newData.name
-      // };
-      //
-      // this.items.update(event.data.$key, editedReminder);
-      //
-      // console.log("on update");
-
-
-      // return event.confirm.resolve(event.data);
-
-  }
-
-  onDelete(event){
-      this.items.remove(event.data);
-      return event.confirm.resolve(event.data);
-  }
-
-  ngOnInit() {
-  }
-
-  logout(){
-    this.authService.logout();
-    this.router.navigate(['login']);
-  }
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['login']);
+    }
 }
